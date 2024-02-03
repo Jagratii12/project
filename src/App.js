e2225c698fbd5cd07fae3435dab9ac25b80dc955
jagrati gera <jagratigera@gmail.com>
2024-02-03 16:23:57 +0530

    first change

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ViewScreen from  './/Component/View.jsx';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <ViewScreen></ViewScreen>
+    </div>
+  );
+}
+
+export default App;
